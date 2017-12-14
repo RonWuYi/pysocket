9521cc4f47e92abf7c307bda4329aa3f54826d99
Ron <lyshmily@outlook.com>
2017-12-14 20:23:55 +0100

    add

--- client1.py
+++ client1.py
@@ -5,32 +5,19 @@ import urllib2
 hh = socket.gethostname()
 
 HOST = socket.gethostbyname(hh)
-# HOST = 'daring.cwi.nl'
 PORT = 50007
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 while True:
-    # s.connect((HOST, PORT))
-    # Cdata = raw_input("please input a message: ")
     if time.localtime()[4] < 10:
         Cdata = str(time.localtime()[3]) + '0' + str(time.localtime()[4])
     else:
         Cdata = str(time.localtime()[3]) + str(time.localtime()[4])
-    # s.sendall(str(time.localtime()[3]) + str(time.localtime()[4]))
     if Cdata == 'q' or Cdata == 'quit':
         break
     else:
         s.sendall(Cdata)
         data = s.recv(1024)
-        # s.close()
-        # print data
-        # time.sleep(2)
-        # if data[:3] == '76':
-        #     break
-        # else:
         print 'Received', repr(data)
-# s.shutdown()
 s.close()
-# time.sleep(2)
-# print 'Received', repr(data)
--- server1.py
+++ server1.py
@@ -2,13 +2,8 @@ import socket
 
 HOST = ''
 PORT = 50007
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.bind((HOST, PORT))
-# s.listen(1)
-# conn, addr = s.accept()
-# print 'Connected by', addr
 
 # def f(x):
 #     return {
@@ -20,24 +15,16 @@ s.listen(5)
 conn, addr = s.accept()
 
 while 1:
-    # s.listen(1)
-    # conn, addr = s.accept()
     try:
         print 'Connected by', addr
         data = conn.recv(1024)
-        # if not data:
-        #     break
-        # elif data == 'close':
-        #     break
         if data == 'q' or data == 'quit':
             break
         else:
             print 'received ' + data + ' from client'
             conn.sendall(data + ' add server part')
-            # print data
     except socket.error as ex:
         print ex
-        # conn.shutdown()
         conn.close()
         s.listen(5)
         conn, addr = s.accept()
@@ -46,24 +33,9 @@ while 1:
 
     except IOError as ex:
         print ex
-        # conn.shutdown()
-        # conn.close()
-        # s.listen(5)
-        # conn, addr = s.accept()
         conn.close()
         s.listen(5)
         conn, addr = s.accept()
         print 'Connected by', addr
         continue
-        # conn.close()
-        # s.listen(1)
-        # conn, addr = s.accept()
-        # while addr:
-        #     continue
-    # except IOError, e:
-    #     if e.errno == error.EPIPE:
-    #         pass
-    #     else:
-    #         pass
-# conn.shutdown()
 conn.close()

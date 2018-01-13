927cbaea0caad3a2d1b6642eb3344fb03df01eac
Ron <lyshmily@outlook.com>
2018-01-13 16:46:09 +0800

    test

--- test1.py
+++ test1.py
@@ -1,12 +1,81 @@
+import time
 from test import AW
 
+CT = (time.localtime()[3]*100) + (time.localtime()[4])
 x = AW()
 
 x.TabQieHuan()
 x.InIt()
-# x.GongXunRenWu(7, 120)
-# x.JingYingRenWu(9, 160)
-# x.ChuangTianGuan(850)
-# x.CaiLiaoFuBen()
-x.GeRenBoss(5)
-x.HuiShou()
+
+while True:
+    x.GongXunRenWu(9, 120)
+    x.JingYingRenWu(9, 160)
+    x.ChuangTianGuan(850)
+    x.CaiLiaoFuBen()
+    x.GeRenBoss(5)
+    x.HuiShou()
+
+while True:
+    if CT > 100 and CT < 130:
+        time.sleep(1)
+        SPlay.InIt()
+        SPlay.TaFangFengMo()
+        SPlay.HuiShou()
+    elif CT > 131 and CT < 220:
+        time.sleep(1)
+        SPlay.InIt()
+        SPlay.GongXunRenWu()
+        SPlay.HuiShou()
+    elif CT > 221 and CT < 300:
+        time.sleep(1)
+        SPlay.InIt()
+        SPlay.JingYingRenWu()
+        SPlay.HuiShou()
+    elif CT > 301 and CT < 330:
+        time.sleep(1)
+        SPlay.InIt()
+        SPlay.CaiLiaoFuBen()
+        SPlay.HuiShou()
+    elif CT > 331 and CT < 410:
+        time.sleep(1)
+        SPlay.ChuMoRenWu()
+        SPlay.HuiShou()
+    elif CT > 411 and CT < 440:
+        time.sleep(1)
+        SPlay.ChuangTianGuan()
+        SPlay.HuiShou()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    # elif CT > 131 and CT < 220:
+    #     time.sleep(1)
+    #     SPlay.GongXunRenWu()
+    while True:
+        SPlay.InIt()
+        SPlay.GuaJi()
+        time.sleep(3600)
+        SPlay.HuiShou()

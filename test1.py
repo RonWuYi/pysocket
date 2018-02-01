3cfe5d322b8a82d894e30f6ef9fc00abc242688f
Ron <lyshmily@outlook.com>
2018-02-02 05:56:31 +0800

    test

--- test1.py
+++ test1.py
@@ -41,10 +41,10 @@ while True:
     elif CT > 100000 and CT < 103000:
         SPlay.RiJinZhuSongLi()
         SPlay.GuaJi(10, 30)
-    elif CT > 103000 and CT < 111000:
+    elif CT > 103000 and CT < 110500:
         SPlay.RiShenWei(30)
         SPlay.GuaJi(13, 0)
-    elif CT > 111000 and CT < 130000:
+    elif CT > 110500 and CT < 130000:
         SPlay.RiDuoBeiYaSong()
         SPlay.GuaJi(13, 0)
     elif CT > 130000 and CT < 140000:

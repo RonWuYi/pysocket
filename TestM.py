b80f1c9ac255a038f2fcbc0949677d99ebd53d2e
Ron <lyshmily@outlook.com>
2018-02-18 17:17:41 +0800

    modified:   TestM.py

--- TestM.py
+++ TestM.py
@@ -4,11 +4,12 @@ from NewAllWay import AW
 from test import AW1
 from test import _re_start
 from test import customize_init
+from test import lian_gong
 
-CT = ((time.localtime()[3] * 10000) + (time.localtime()[4] * 100) + (time.localtime()[5]))
-# print CT
-SPlay = AW()
-SPlayN = AW1(2)
+# CT = ((time.localtime()[3] * 10000) + (time.localtime()[4] * 100) + (time.localtime()[5]))
+# # print CT
+# SPlay = AW()
+SPlay = AW1(0,81)
 time.sleep(2)
 #
 # print datetime.now()
@@ -20,4 +21,15 @@ time.sleep(2)
 # customize_init()
 # _go_gua_ji_npc()
 # _re_start()
-SPlayN.cai_liao_fu_ben(2, 3, 75, 30)
+SPlay.gong_xun_ren_wu(run_times=10, extra_time=110, ge_su=3)
+SPlay.jing_ying_ren_wu(run_times=10, extra_time=125, ge_su=5)
+SPlay.chuang_tian_guan(120)
+SPlay.hui_shou_full()
+SPlay.cai_liao_fu_ben(which_one=4, extra_time=50)
+SPlay.hui_shou_full()
+SPlay.ge_ren_boss(120)
+SPlay.hui_shou_full()
+SPlay.ta_fang_feng_mo(3)
+SPlay.wei_wang_ren_wu()
+SPlay.chu_mo_ren_wu(45, False)
+lian_gong(45)

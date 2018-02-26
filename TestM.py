59a823ab42fadf3cde3f26f70dd24d4699e07a22
Ron <lyshmily@outlook.com>
2018-02-26 03:56:19 -0800

    modified:   TestM.py

--- TestM.py
+++ TestM.py
@@ -1,37 +1,31 @@
 import time
-from datetime import datetime
-from NewAllWay import AW
 from test import AW1
-from test import seconds_change
 from test import customize_init
+from test import lin_qu_huo_yue_jiang_li
 from test import lian_gong
 
-# CT = ((time.localtime()[3] * 10000) + (time.localtime()[4] * 100) + (time.localtime()[5]))
-# # print CT
-# SPlay = AW()
-SPlay = AW1(0,81)
-# time.sleep(2)
-#
-# print datetime.now()
-#
+SPlay = AW1(2, 81)
+SPlay.tab_qie_huan()
+customize_init()
 
-# print seconds_change(SPlayN._time_diff(23, 59))
-# print type(SPlayN.SecondsChange(SPlayN._TimeDiff(19, 5)))
-# #
-# customize_init()
-# _go_gua_ji_npc()
-# _re_start()
-# SPlay.gong_xun_ren_wu(run_times=10, extra_time=110, ge_su=3)
-# SPlay.jing_ying_ren_wu(run_times=10, extra_time=125, ge_su=5)
-# SPlay.chuang_tian_guan(120)
-# SPlay.hui_shou_full()
-# SPlay.cai_liao_fu_ben(which_one=4, extra_time=50)
-# SPlay.hui_shou_full()
-# SPlay.ge_ren_boss(120)
-# SPlay.hui_shou_full()
-# SPlay.ta_fang_feng_mo(3)
-# SPlay.wei_wang_ren_wu()
-# SPlay.chu_mo_ren_wu(45, False)
-# lian_gong(45)
-
-print seconds_change(SPlay._time_diff(hh=1, mm=20))
+while True:
+    SPlay = AW1(2, 81)
+    time.sleep(5)
+    SPlay.gong_xun_ren_wu(run_times=10, extra_time=40, ge_su=3)
+    SPlay.jing_ying_ren_wu(run_times=10, extra_time=60, ge_su=5)
+    SPlay.chuang_tian_guan(160)
+    SPlay.hui_shou_full()
+    SPlay.cai_liao_fu_ben(which_one=7, extra_time=50)
+    SPlay.hui_shou_full()
+    SPlay.ge_ren_boss(160)
+    SPlay.hui_shou_full()
+    SPlay.ta_fang_feng_mo(3)
+    SPlay.wei_wang_ren_wu()
+    SPlay.chu_mo_ren_wu(45, False)
+    lian_gong(45)
+    SPlay.gua_ji(hh=10, mm=0, ss=0)
+    time.sleep(1)
+    SPlay.hui_shou_full()
+    SPlay.gua_ji(hh=23, mm=58)
+    lin_qu_huo_yue_jiang_li()
+    SPlay.hui_shou_full()

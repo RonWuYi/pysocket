b83d1d3776271b19fe120c045d84829f22e30929
Ron <lyshmily@outlook.com>
2018-03-23 21:05:25 +0800

    modified:   TestM.py

--- TestM.py
+++ TestM.py
@@ -5,8 +5,8 @@ from test import lian_gong
 while True:
     SPlay = AW1(2, 81)
     time.sleep(5)
-    SPlay.gong_xun_ren_wu(run_times=10, extra_time=40, ge_su=3)
-    SPlay.jing_ying_ren_wu(run_times=10, extra_time=60, ge_su=7)
+    SPlay.gong_xun_ren_wu(run_times=10, extra_time=5, ge_su=3)
+    SPlay.jing_ying_ren_wu(run_times=10, extra_time=15, ge_su=6)
     SPlay.chuang_tian_guan(160)
     SPlay.cai_liao_fu_ben(fu_ben_time=2, which_one=7, extra_time=50)
     SPlay.ge_ren_boss(160)

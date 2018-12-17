62e746accc813fc6d8edf15dd373f40fc8fbaac0
Ron <lyshmily@outlook.com>
2018-12-18 06:39:51 +0800

    add new branch

---
+++ Package/restart.py
@@ -0,0 +1,27 @@
+import pywinauto
+
+class Base(object):
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
+    def do(self):
+        pass
+
+    def close(self):
+        pass
+
+    def clean(self):
+        pass
+
+    
+class Restart(Base):
+    def __init__(self):
+        Base.__init__(self)
+
+
+
+
+

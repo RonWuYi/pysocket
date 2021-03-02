dc6fd3fc2fa6e5bacdf863003a49ed87f9e99cdc
RonWuYi <lyshmily@outlook.com>
2021-03-02 10:04:38 +0100

    update

--- 7.py
+++ 7.py
@@ -1,10 +1,2 @@
-import os,time
-import pyautogui as pag
-
-while True:
-      x,y = pag.position()
-      pos="Position:"+str(x).rjust(4)+','+str(y).rjust(4)
-      print pos
-      time.sleep(1)
-      os.system('cls')
-# print 'ending   ....'
+import os
+print(os.getcwd())
